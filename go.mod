04ed77d6970df41399aafba1e30913490c644133
ganganlee <ganganlee@outlook.com>
2020-04-07 23:23:16 +0800

    加入viper 读取配置文件

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module shop.zozoo.net
 
 go 1.13
 
-require github.com/gin-gonic/gin v1.6.2
+require (
+	github.com/gin-gonic/gin v1.6.2
+	github.com/spf13/viper v1.6.2
+)
--- main.go
+++ main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 	"net/http"
+	"shop.zozoo.net/config"
 )
 
 //初始化项目
-func main(){
-	r:= gin.Default()
+func main() {
+	r := gin.Default()
+
+	//初始化配置文件
+	err := config.InitConfig()
+	if err != nil {
+		panic(err)
+	}
 
 	//设置静态目录
 	r.Static("/static", "./static")
@@ -16,41 +24,41 @@ func main(){
 
 	//首页
 	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK,"index.html",gin.H{"title":"gin Shop"})
+		c.HTML(http.StatusOK, "index.html", gin.H{"title": "gin Shop"})
 	})
 	//登陆页
 	r.GET("/login", func(c *gin.Context) {
-		c.HTML(http.StatusOK,"login.html",gin.H{"title":"gin Shop"})
+		c.HTML(http.StatusOK, "login.html", gin.H{"title": "gin Shop"})
 	})
 	//关于我们
 	r.GET("/about", func(c *gin.Context) {
-		c.HTML(http.StatusOK,"about.html",gin.H{"title":"gin Shop"})
+		c.HTML(http.StatusOK, "about.html", gin.H{"title": "gin Shop"})
 	})
 
 	//关于我们
 	r.GET("/details", func(c *gin.Context) {
-		c.HTML(http.StatusOK,"details.html",gin.H{"title":"gin Shop"})
+		c.HTML(http.StatusOK, "details.html", gin.H{"title": "gin Shop"})
 	})
 
 	//购物车
 	r.GET("/shopCart", func(c *gin.Context) {
-		c.HTML(http.StatusOK,"shopCart.html",gin.H{"title":"gin Shop"})
+		c.HTML(http.StatusOK, "shopCart.html", gin.H{"title": "gin Shop"})
 	})
 
 	//母婴资讯
 	r.GET("/information", func(c *gin.Context) {
-		c.HTML(http.StatusOK,"information.html",gin.H{"title":"gin Shop"})
+		c.HTML(http.StatusOK, "information.html", gin.H{"title": "gin Shop"})
 	})
 
 	//购物车
 	r.GET("/buyCart", func(c *gin.Context) {
-		c.HTML(http.StatusOK,"buyCart.html",gin.H{"title":"gin Shop"})
+		c.HTML(http.StatusOK, "buyCart.html", gin.H{"title": "gin Shop"})
 	})
 
 	//商品分类
 	r.GET("/commotidy", func(c *gin.Context) {
-		c.HTML(http.StatusOK,"commotidy.html",gin.H{"title":"gin Shop"})
+		c.HTML(http.StatusOK, "commotidy.html", gin.H{"title": "gin Shop"})
 	})
 
-	r.Run()
+	r.Run(viper.GetString("server.port"))
 }
